6cce4e44502b998a8bec080f19e200fa5192d2e2
thoppe <thomas.hoppe@n-fuse.de>
2013-02-23 15:43:13 +0100

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+pointeraccuracy.js
+==============
+
+Heuristically determine the pointer accuracy &mdash; 'coarse' or 'fine' &mdash; for a device/ user agent.
+
+# Motivation
+The 'pointer' media query as defined in level 4 (http://dev.w3.org/csswg/mediaqueries4/#pointer) can provide a valuable indication 
+for responsive decision making for rendering of input widgets but also content in general. 
+For example a date picker widget might be rendered in a touch _or_ mouse/ stylus optimized way.
+Because the W3C recommendation is currently in draft status, and browser
+support is naturally low we've created this library.
+
+# Heuristic
+The classifier uses the following four properties of the current device to make a decision:
+
+* Pointer media query result 
+* Screen resolution
+* Device pixel ratio
+* Touch input capability
+
+Some of these properties cannot reliably determined &mdash; "best effort" is the right way to deal with this.
+
+The algorithm then is:
+
+1.	If the pointer media query is supported, and the result is not 'none', use it
+2.	If a screen with device pixel ratio 2 is used, use 'coarse'
+3.	If we have a small screen resolution (< 1024px) use 'coarse'
+4.	If we have a medium screen resolution (>= 1024px, <=1280)
+	- If we have touch capability, use 'coarse'
+	- If we have no touch capability, use 'fine'
+5. 	Otherwise, use 'fine'
+
+# Usage
+See (demo.html)
+
+## Dependencies
+* A polyfill for ES5 Object.create() must exist for older browsers.
+
+
+# License
+[MIT License] (LICENSE.txt)
---
+++ pointeraccuracy.src.js
@@ -0,0 +1,129 @@
+/**
+ * pointeraccuracy.js: Heuristically determine the pointer accuracy -- 'coarse' or 'fine' -- for a device/ user agent.
+ * 
+ * If rendering for a component, widget etc. should be depending on the accuracy of the pointing device,
+ * the level 4 media query '@media (pointer:coarse)' is handy.
+ * Browser support is low for this MQ but this classifier allows you to still get the desired information.
+ * It resembles this media query heuristically based on the following properties:
+ * - Pointer media query result (if available)
+ * - Screen resolution
+ * - Device pixel ratio
+ * - Touch capability
+ * 
+ * @copyright n-fuse GmbH [All rights reserved]
+ * @version 0.1.0
+ * @license pointeraccuracy.js is (c) 2013 n-fuse GmbH [All rights reserved] and licensed under the MIT license.
+ */
+
+"use strict";
+
+var Pointeraccuracy = {
+		listener: null,
+		resolution: null,
+		pointerModeNative: null,
+		pointerMode: null,
+		init: function(cb) {
+			var thiz = this;
+			thiz.listener = cb;
+			
+			// Find out whether the browser supports the pointer mode media query and if so, get it
+			if (window.matchMedia('(pointer:fine)').matches) {
+				thiz.pointerModeNative = 'fine'; // mouse, stylus, ...
+			} else if (window.matchMedia('(pointer:coarse)').matches) {
+				thiz.pointerModeNative = 'coarse'; // touch screen
+			}
+
+			console.log('NATIVE:' + thiz.pointerModeNative);
+
+			// The viewport size may change in a desktop environment, therefore we need to listen to changes.
+			window.onresize = function(e) {
+				thiz.classifyScreenSize();
+				thiz.classify();
+			};
+			// Initial classification		
+			thiz.classifyScreenSize();
+			thiz.classify();
+		},
+		
+		// The classifier returns either 'fine' or 'coarse' 
+		// thus canceling out 'none' and preventing 'undefined' which are useless in many cases.
+		classify: function() {
+			var thiz = this;
+			var mode;
+			if (thiz.pointerModeNative !== null) { // If pointer MQ is supported, just use its result
+				mode = pointerModeNative;
+			} else {
+				if (thiz.hasDoubleDevicePixelRatio()) { // Screens with DPR = 2 can these days only be found on small-medium screen touch devices 
+					mode = 'coarse';
+				} else {
+					if (thiz.resolution === 'small') { // Small screens 
+						mode = 'coarse';
+					} else if (thiz.resolution === 'medium') {
+						if (thiz.hasTouchSupport()) {
+							mode = 'coarse';
+						} else {
+							mode = 'fine';
+						}
+					} else { // Large resolutions
+						mode = 'fine';
+					}
+				}
+			}
+			thiz.setPointerMode(mode);
+		},
+		
+		classifyScreenSize: function() {
+			var thiz = this;
+			if (window.matchMedia('screen and (max-width: 1023px)').matches) {
+				thiz.resolution = 'small';
+			} else if (window.matchMedia('screen and (min-width: 1024px) and (max-width: 1280px)').matches) {
+				thiz.resolution = 'medium';
+			} else if (window.matchMedia('screen and (min-width: 1281px)').matches) {
+				thiz.resolution = 'large';
+			}			
+		},
+		
+		setListener: function(cb) {
+			var thiz = this;
+			thiz.listener = cb;
+		},
+		
+		setPointerMode: function(mode) {
+			var thiz = this;
+			if (thiz.pointerMode !== mode) {
+				thiz.pointerMode = mode;
+				if (typeof(thiz.listener) === 'function') {
+					thiz.listener(mode);
+				}
+			}
+			console.log('Using pointer mode: ' + mode);
+		},
+		
+		getPointerMode: function() {
+			return thiz.pointerMode;
+		},
+		
+		hasTouchSupport: function() {
+			return !!('ontouchstart' in window) // works on most browsers
+				|| !!('onmsgesturechange' in window); // works on IE10
+		},
+		
+		hasDoubleDevicePixelRatio: function() {
+			var retVal = false;
+			if (window.matchMedia('screen and (-webkit-min-device-pixel-ratio: 2)').matches ||
+				window.matchMedia('screen and (min-device-pixel-ratio: 2)').matches ||
+				window.matchMedia('screen and (min-resolution: 2dppx)').matches ||
+				window.matchMedia('screen and (-o-min-device-pixel-ratio: 2/1)').matches) {
+				retVal = true;
+			}
+			return retVal;
+		},
+
+		isPointerFine: function() {
+			return this.pointerMode === 'fine';
+		},
+
+		isPointerCoarse: function() {
+			return this.pointerMode === 'coarse';
+		}
+};
